c2476411f2b9e4cfa6a0c0424f545028a2fe624a
Erik Xu <81636759@qq.com>
2022-03-14 09:48:25 +0800

    Add pack.sh.

---
+++ pack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+IMAGE_NAME=${IMAGE_NAME:-vs-conf-operator}
+echo "IMAGE_NAME: "$IMAGE_NAME
+
+IMAGE_TAG=${IMAGE_TAG:-1.0.0}
+echo "IMAGE_TAG: "$IMAGE_TAG
+
+docker build -t ${IMAGE_NAME} .
+#docker push ${IMAGE_NAME}:${IMAGE_TAG}
+#docker rmi ${IMAGE_NAME}:${IMAGE_TAG}
