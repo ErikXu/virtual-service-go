f00da65723020897ce7673118e86cd53bd1f9f15
Erik Xu <81636759@qq.com>
2022-03-14 09:52:05 +0800

    Fix pack name.

--- pack.sh
+++ pack.sh
@@ -6,6 +6,6 @@ echo "IMAGE_NAME: "$IMAGE_NAME
 IMAGE_TAG=${IMAGE_TAG:-1.0.0}
 echo "IMAGE_TAG: "$IMAGE_TAG
 
-docker build -t ${IMAGE_NAME} .
+docker build -t ${IMAGE_NAME}:${IMAGE_TAG} .
 #docker push ${IMAGE_NAME}:${IMAGE_TAG}
 #docker rmi ${IMAGE_NAME}:${IMAGE_TAG}
